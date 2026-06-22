f47f26479b3c8d67357adab67020ee184091625e
32ndbatch17c33-maker <32ndbatch17c33@gmail.com>
2026-06-22 14:22:47 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+{
+  "short_name": "MyPWA",
+  "name": "My Progressive Web App",
+  "icons": [
+    {
+      "src": "./icon-192.png",
+      "type": "image/png",
+      "sizes": "192x192",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "./icon-512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "./index.html",
+  "background_color": "#FFFFFF",
+  "theme_color": "#000000",
+  "display": "standalone",
+  "orientation": "portrait"
+}
